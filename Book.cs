79e340dd43ad724b9bc98a388877f60f5eb81eb7
Lauravilen <44665204+Lauravilen@users.noreply.github.com>
2021-10-10 14:47:47 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BookLibrary
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int NumberOfPages { get; set; }
+        public string ISBN13 { get; set; }
+
+        public Book()
+        {
+            
+        }
+
+        public Book(string title, string author, int numberOfPages, string isbn13)
+        {
+            Title = title;
+            Author = author;
+            NumberOfPages = numberOfPages;
+            ISBN13 = isbn13;
+        }
+    }
+}
