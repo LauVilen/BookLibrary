4425ad01e7388764e985f84667a2e13bf1c7d618
Lauravilen <44665204+Lauravilen@users.noreply.github.com>
2021-10-10 15:38:20 +0200

    Added summeries to the Book class

--- Book.cs
+++ Book.cs
@@ -4,11 +4,26 @@ namespace BookLibrary
 {
     public class Book
     {
+        /// <summary>
+        /// Gets or sets the title of the book as a string
+        /// </summary>
         public string Title { get; set; }
+        /// <summary>
+        /// Gets or sets the name of the Author of the book as a string
+        /// </summary>
         public string Author { get; set; }
+        /// <summary>
+        /// Gets or sets the number of pages in the book as an integer
+        /// </summary>
         public int NumberOfPages { get; set; }
+        /// <summary>
+        /// Gets or sets the ISBN-13 number of the book as a string
+        /// </summary>
         public string ISBN13 { get; set; }
 
+        /// <summary>
+        /// Represents a book with a unique ISBN-13 number
+        /// </summary>
         public Book()
         {
             
