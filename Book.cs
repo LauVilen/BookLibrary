15348ea4cd304fc684a37d9f2d2c3862e401162d
Lauravilen <44665204+Lauravilen@users.noreply.github.com>
2021-10-10 19:47:00 +0200

    ...

--- Book.cs
+++ Book.cs
@@ -2,8 +2,14 @@
 
 namespace BookLibrary
 {
+    /// <summary>
+    /// Represents a book with a unique ISBN-13 number
+    /// </summary>
     public class Book
     {
+        /// Gets or sets the ISBN-13 number of the book as a string
+        /// </summary>
+        public string ISBN13 { get; set; }
         /// <summary>
         /// Gets or sets the title of the book as a string
         /// </summary>
@@ -17,24 +23,29 @@ namespace BookLibrary
         /// </summary>
         public int NumberOfPages { get; set; }
         /// <summary>
-        /// Gets or sets the ISBN-13 number of the book as a string
-        /// </summary>
-        public string ISBN13 { get; set; }
+        
 
         /// <summary>
-        /// Represents a book with a unique ISBN-13 number
+        /// Creates an instance of a book with an empty constructor
         /// </summary>
         public Book()
         {
             
         }
 
-        public Book(string title, string author, int numberOfPages, string isbn13)
+        /// <summary>
+        /// Creates an instance of a book with four parameters to set Title, Author, Number of Pages and the ISBN number
+        /// </summary>
+        /// <param name="isbn13"></param>
+        /// <param name="title"></param>
+        /// <param name="author"></param>
+        /// <param name="numberOfPages"></param>
+        public Book(string isbn13, string title, string author, int numberOfPages)
         {
+            ISBN13 = isbn13;
             Title = title;
             Author = author;
             NumberOfPages = numberOfPages;
-            ISBN13 = isbn13;
         }
     }
 }
